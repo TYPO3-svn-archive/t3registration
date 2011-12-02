b8b88e7654140b1dfcbbef7a1056420a6888d3d1
federico@bernardin.it <federico@bernardin.it@735d13b6-9817-0410-8766-e36946ffe9aa>
2011-12-02 11:03:53 +0000

    first TER version
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/t3registration/trunk@55254 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ ext_localconf.php
@@ -0,0 +1,9 @@
+<?php
+if (!defined('TYPO3_MODE')) {
+  die ('Access denied.');
+}
+
+t3lib_extMgm::addPItoST43($_EXTKEY, 'pi1/class.tx_t3registration_pi1.php', '_pi1', 'list_type', 0);
+$TYPO3_CONF_VARS['EXTCONF']['t3registration']['extraMarkersRegistration'][] = 'EXT:t3registration/hooks/class.tx_t3registration_hooks.php:tx_t3registration_hooks->addPasswordMarker';
+$TYPO3_CONF_VARS['EXTCONF']['t3registration']['profileFetchData'][] = 'EXT:t3registration/hooks/class.tx_t3registration_hooks.php:tx_t3registration_hooks->fillPasswordFieldForProfile';
+?>
